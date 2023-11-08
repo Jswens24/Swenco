f0533d6844158a33ce7d1fd2826787f52b22f36b
Jessica Swenson <jessicamswens@gmail.com>
2023-11-08 16:39:55 -0700

    added collapsible div

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 
 const Services = () => {
     const [isCollapsed, setIsCollapsed] = useState(true);
+
+    function handleClick() {
+        setIsCollapsed(!isCollapsed);
+    }
+
     return (
         <div>
             <h1>Our Services</h1>
-            <div>
-                <button onClick={() => setIsCollapsed(false)}>{isCollapsed ? 'Expand' : 'Collapse'}</button>
-                <p className={isCollapsed ? 'hidden' : ''}>Now you see me</p>
-            </div>
+            <button onClick={handleClick}>
+                {isCollapsed ? '▼ Residential Remodeling' : '► Residential Remodeling'}
+            </button>
+            {isCollapsed && <p>Transform your existing home into your dream space with our residential remodeling services. We specialize in kitchen and bathroom renovations, basement finishing, and more, ensuring that your living environment suits your evolving needs and style.</p>}
+
         </div>
-    )
+    );
 }
 
 export default Services
